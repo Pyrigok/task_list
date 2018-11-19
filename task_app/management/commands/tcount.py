26d2ff61e307920b4bbadac670f5f45f94b41ecb
Vasyl Pyrig <pyrigok@i.ua>
2018-11-19 15:49:44 +0200

    Count command was changed

--- task_app/management/commands/tcount.py
+++ task_app/management/commands/tcount.py
@@ -1,16 +1,20 @@
 from django.core.management.base import BaseCommand
 
 from task_app.models import Task_Details_Model
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
 	args = '<model_name model_name ...>'
 	help = "Prints to console number of task in database."
 
+	models = (('task', Task_Details_Model), ('user', User))
+
 	def add_arguments(self, parser):
 		parser.add_argument('task', type=str)
+		parser.add_argument('user', type=str)
 
 	def handle(self, *args, **options):
-		if 'task' in options:
-			self.stdout.write('Number of tasks in DB: %d' % Task_Details_Model.objects.count())
-			#return Task_Details_Model.objects.count()
+		for name, model in self.models:
+			if name in options:
+				self.stdout.write('Number of %ss in DB: %d' % (name, model.objects.count()))
